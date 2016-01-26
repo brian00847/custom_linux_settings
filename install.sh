ef712a446b4b48681da8b4cb2a12574c4635f816
bhenkel <brian00847>
2016-01-27 00:38:34 +0100

    adding install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+function link
+{
+    arg_file=${1}
+    bak_file=${arg_file}.bak
+    pushd ~/
+    if [ -f ${arg_file} -a ! -h ${arg_file} ]; then
+        echo "Moving ${arg_file} to ${bak_file}"
+        mv ${arg_file} ${bak_file}
+    fi
+    popd
+    echo "Creating symlink in home directory for $(pwd)/${arg_file}"
+    ln -sf $(pwd)/${arg_file} ~/
+}
+
+
+link .vimrc
+link .tmux.conf
+
+
