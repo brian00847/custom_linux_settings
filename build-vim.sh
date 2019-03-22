fcf27084b5b852171147e8e3eee0626689886a74
bhenkel <bhenkel@localhost.localdomain>
2019-03-22 16:35:51 -0500

    updating vim build flags

--- build-vim.sh
+++ build-vim.sh
@@ -3,6 +3,7 @@
 #Enable copying to clipboard with these rpm packages:
 #install these rpms libX11-devel libXtst-devel libXt-devel libSM-devel libXpm-devel
 #Install python2-devel and python3-devel
+#For GVIM gtk2-devel
 
 #Note: Manually installing command-t didn't seem to work too well.
 #Look into running vundle through command-line
@@ -32,9 +33,12 @@ cd vim-master
 rm -f src/auto/config.cache
 VIMPYFLAGS="--enable-pythoninterp=yes   --with-python-config-dir=/usr/lib64/python2.7/config/"
 VIMPY3FLAGS="--enable-python3interp=yes --with-python3-config-dir=/usr/lib64/python3.6/config/"
-CFLAGS="${CFLAGS} -fPIC -O3" ./configure --prefix=${HOME}/vim-8.x/ --enable-rubyinterp=yes ${VIMPYFLAGS} ${VIMPY3FLAGS}
+GUI_FLAGS="--enable-gui=gtk2 --enable-gnome-check --with-x" #This works!! with gtk2-devel installed
+CFLAGS="${CFLAGS} -fPIC -O3" ./configure --prefix=${HOME}/vim-8.x/ --enable-rubyinterp=yes ${VIMPYFLAGS} ${VIMPY3FLAGS} ${GUI_FLAGS}
 make -j4 install
 echo "Current directory is $(pwd)"
 
 popd
+
+echo "Extract dir is: ${extract_dir}"
 rm -rf $extract_dir
