c4d2ca59e73c4fe065446bae5d2856584a3d082b
bh <bh@bh.net>
2024-07-18 12:59:31 -0500

    updating log1.hpp file

--- log1/log1.hpp
+++ log1/log1.hpp
@@ -1,26 +1,75 @@
+#ifndef LOG1_HPP__
+#define LOG1_HPP__
+
 #include <cstdio>
 #include <cstdarg>
 #include <cstring>
 
-void logMessage2(const char* loglevel, const char* filename, const int line, const char* format, va_list argp)
+#define LOGLEVEL_DBG 0
+#define LOGLEVEL_INFO 1
+#define LOGLEVEL_WARN 2
+#define LOGLEVEL_ERR 3
+
+static
+const char* getFilename(const char* filepath)
+{
+    if (filepath == nullptr)
+    {
+        return nullptr;
+    }
+
+    // Find the last occurrence of '/' or '\'
+    const char* lastSlash = std::strrchr(filepath, '/');
+    const char* lastBackslash = std::strrchr(filepath, '\\');
+
+    // Determine which is the actual last separator
+    const char* lastSeparator = lastSlash > lastBackslash ? lastSlash : lastBackslash;
+
+    // If no separator is found, return the whole filepath as the filename
+    if (lastSeparator == nullptr)
+    {
+        return filepath;
+    }
+    else
+    {
+        // Return the pointer to the character after the last separator
+        return lastSeparator + 1;
+    }
+}
+
+static
+void logMessage2(const char* loglevel, const char* filepath, const int line, const char* funcname, const char* format, va_list argp)
 {
     char buff[1024];
-    sprintf(buff, "%s:%d:%s: ", filename, line, loglevel);
-    printf("%s\n", buff);
+    sprintf(buff, "%-20s:%04d:%-4s: ", getFilename(filepath), line, loglevel);
+    //printf("%s\n", buff);
 
     vsprintf(&buff[strlen(buff)], format, argp);
     printf("%s\n", buff);
 
 }
 
-void logMessage1(const char* filename, const int line, const char* format, ...)
+static
+void logMessage1(const char* filename, const int line, const int loglevel, const char* funcname, const char* format, ...)
 {
     va_list argp;
     va_start(argp, format);
+    const char* loglevel_str = nullptr;
+    switch (loglevel)
+    {
+        case LOGLEVEL_DBG: loglevel_str = "DBG"; break;
+        case LOGLEVEL_WARN: loglevel_str = "WARN"; break;
+        case LOGLEVEL_ERR: loglevel_str = "ERR"; break;
+        default:
+                           loglevel_str = "INFO"; break;
+    }
 
-    logMessage2("INFO", filename, line, format, argp);
+    logMessage2(loglevel_str, filename, line, funcname, format, argp);
     va_end(argp);
 }
 
 
-#define BHLOG(...) logMessage1(__FILE__, __LINE__, __VA_ARGS__)
+#define BHLOG_INFO(...) logMessage1(__FILE__, __LINE__, LOGLEVEL_INFO, __FUNCTION__, __VA_ARGS__)
+#define BHLOG_ERR(...) logMessage1(__FILE__, __LINE__, LOGLEVEL_ERR, __FUNCTION__, __VA_ARGS__)
+
+#endif
