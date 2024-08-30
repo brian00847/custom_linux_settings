3263bff0f9de1c0dd1c95f7f815ce67344badfa4
bh <bh@bh.net>
2024-08-30 16:29:14 -0500

    Adding support for << operator

--- log1/log1.hpp
+++ log1/log1.hpp
@@ -4,6 +4,8 @@
 #include <cstdio>
 #include <cstdarg>
 #include <cstring>
+#include <iostream>
+#include <sstream>
 
 #define LOGLEVEL_DBG 0
 #define LOGLEVEL_INFO 1
@@ -41,7 +43,7 @@ static
 void logMessage2(const char* loglevel, const char* filepath, const int line, const char* funcname, const char* format, va_list argp)
 {
     char buff[1024];
-    sprintf(buff, "%-20s:%04d:%-4s: ", getFilename(filepath), line, loglevel);
+    sprintf(buff, "%-20s : %04d : %-4s : %-25s: ", getFilename(filepath), line, loglevel, funcname);
     //printf("%s\n", buff);
 
     vsprintf(&buff[strlen(buff)], format, argp);
@@ -68,8 +70,42 @@ void logMessage1(const char* filename, const int line, const int loglevel, const
     va_end(argp);
 }
 
+class BHLogger {
+public:
+    BHLogger(const char* filename, int line, int loglevel, const char* funcname)
+        : filename_(filename), line_(line), loglevel_(loglevel), funcname_(funcname) {}
+
+    ~BHLogger() {
+        // Output the collected log message when the logger is destroyed
+        logMessage1(filename_, line_, loglevel_, funcname_, stream_.str().c_str());
+    }
+
+    // Overload the << operator to accept various types
+    template<typename T>
+    BHLogger& operator<<(const T& msg) {
+        stream_ << msg;
+        return *this;
+    }
+
+        // Overload the << operator for manipulators
+    typedef std::ostream& (*StreamManipulator)(std::ostream&);
+    BHLogger& operator<<(StreamManipulator manip) {
+        manip(stream_);
+        return *this;
+    }
+
+private:
+    std::ostringstream stream_;
+    const char* filename_;
+    int line_;
+    int loglevel_;
+    const char* funcname_;
+};
+
 
 #define BHLOG_INFO(...) logMessage1(__FILE__, __LINE__, LOGLEVEL_INFO, __FUNCTION__, __VA_ARGS__)
-#define BHLOG_ERR(...) logMessage1(__FILE__, __LINE__, LOGLEVEL_ERR, __FUNCTION__, __VA_ARGS__)
+#define BHLOG_ERR(...)  logMessage1(__FILE__, __LINE__, LOGLEVEL_ERR, __FUNCTION__, __VA_ARGS__)
+#define BHLOG_WARN(...) logMessage1(__FILE__, __LINE__, LOGLEVEL_WARN, __FUNCTION__, __VA_ARGS__)
 
+#define BHLOG_INFO2 BHLogger(__FILE__, __LINE__, LOGLEVEL_INFO, __FUNCTION__)
 #endif
