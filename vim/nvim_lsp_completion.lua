cdc02735c2b68bed781ee5b9e8f51b6463bded18
bh <bh@bh.net>
2025-03-25 18:28:19 -0500

    neovim: adding c++ autocomplete support. adding command-t

--- vim/nvim_lsp_completion.lua
+++ vim/nvim_lsp_completion.lua
@@ -1,13 +1,19 @@
 
 
 
+
 -- Installing LSP + nvim-cmp for autocompletion
 --      -Install lazy.nvim
 --      -Install pyright language server.
 --      -Use lazy.nvim to install 'nvim-cmp'
 
+
+
 -- Set runtime path for lazy.nvim
 vim.opt.rtp:prepend("{{ lazy_nvim_dir }}")
+
+-- Getting autocompletion to work for C++
+-- sudo apt install clangd
 
 -- Ensure lazy.nvim is properly loaded.  Note: had to manually add this path
 local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
@@ -30,14 +36,18 @@ require("lazy").setup({
     { "hrsh7th/cmp-path" },         -- Path completion
     { "hrsh7th/cmp-cmdline" },      -- Command-line completion
     { "L3MON4D3/LuaSnip" },         -- Snippets engine
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" } -- Syntax highlighting
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },  -- Syntax highlighting
+    { "wincent/command-t"} -- Command-T
 })
 
 -- LSP and nvim-cmp setup
 local lspconfig = require('lspconfig')
 local cmp = require('cmp')
 
+-- Setting up pyright for python autocomplete
 lspconfig.pyright.setup{}
+-- Setting up clangd for C++ autocomplete. Requires clangd to be installed
+lspconfig.clangd.setup{}
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
